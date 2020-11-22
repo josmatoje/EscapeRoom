605927148357a467af91cd759863796f6007ec0a
josmatoje <josemariamataojeda@gmail.com>
2020-11-22 10:50:02 +0100

    subprogramas de mensajes y declaracion de algunas  variables

--- EscapeRoom/src/main/EscapeRoom.java
+++ EscapeRoom/src/main/EscapeRoom.java
@@ -10,16 +10,18 @@ public class EscapeRoom {
         //Declaracion de variables
         boolean respuestaSiNo;
         int zona=0;
+        String objeto1="", objeto2="";//Declaracion de todos los objetos
+        String[] objetos= new String[10];
         
         Comprobaciones comp = new Comprobaciones();
         Mensajes mensaje = new Mensajes();
         
-        mensaje.mensajeBienvenida();
+        mensaje.Bienvenida();
         
         //do-while del escape room completo
         do{
             
-            mensaje.mensajeInicio();
+            mensaje.Inicio();
             
             //If-else de dificultad
             int movimientos = 120, vida = 100;  //movimiento por minuto vida puede no ser necesaria(?) por como planteamos el juego
@@ -29,14 +31,17 @@ public class EscapeRoom {
             //Vida inicial y movimientos
             System.out.println("");
 
-            mensaje.mensajesMenu(zona);
+            mensaje.Menu(zona);
         
+            
+            
+            System.out.println("Desea volver a intentarlo?");
             respuestaSiNo = comp.validacionSiNo();
             
         }while(respuestaSiNo);
         
-        System.out.println("Esperamos que vuelva pronto.");
-        System.out.println("¡Que la fuerza te acompañe!");
+        mensaje.Salida();
+        
         
     }
     
